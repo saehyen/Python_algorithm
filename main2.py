563e2d74d0a5c3edba417e59339b18c56104f05e
saehyen <rlatpgus4564@google.com>
2021-05-19 22:42:03 +0900

    설명추가

--- main2.py
+++ main2.py
@@ -1,9 +1,15 @@
 n,m = map(int,input().split())
 result = 0
-
+#한 줄씩 입력받아 확인
 for i in range(n) :
     data = list(map(int,input().split()))
+#현재 줄에서 가장 작은 수
     min_value = min(data)
+#가장 작은 수들 중에서 가장 큰 수 찾기 (결과값 비교)
     result = max(result, min_value)
 
 print(result)
+
+#list(map(int,input().split()))
+#min(리스트)
+#max(비교값,비교값)
