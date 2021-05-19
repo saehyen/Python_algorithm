16bad2f19e5348a5c0047ebd5586a10489105f3a
saehyen <rlatpgus4564@google.com>
2021-05-19 22:40:40 +0900

    답

--- main2.py
+++ main2.py
@@ -1,1 +1,9 @@
-print("hi")
+n,m = map(int,input().split())
+result = 0
+
+for i in range(n) :
+    data = list(map(int,input().split()))
+    min_value = min(data)
+    result = max(result, min_value)
+
+print(result)
