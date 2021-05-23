4112a438f6152b9197329a447383971033a21449
saehyen <rlatpgus4564@google.com>
2021-05-23 20:00:10 +0900

    수정 및 정답

--- rope.py
+++ rope.py
@@ -6,16 +6,15 @@ while n :
     n -= 1
 
 k.sort(reverse=(True))
-sum = 0
+
 num = 0
 result = 0
 max = 0
 for i in k :
     num += 1
-    sum += i
     result = num * i
+
     if result > max :
         max = result
 
-print(result)
-print(r)
+print(max)
