e1e159895a833eea11dec0c335eaf1633079ee38
saehyen <79187118+saehyen@users.noreply.github.com>
2021-05-26 22:21:43 +0900

    Delete MeetingRoom2.py

--- MeetingRoom2.py
+++
@@ -1,8 +0,0 @@
-n = int(input())
-time = sorted([tuple(map(int, input().split()))for _ in range(n)], key=lambda x:(x[1], x[0]))
-ans = end = 0
-for s, e in time:
-    if s >= end:
-        ans += 1
-        end = e
-        print(ans)
