db673ef427f836575675547852311c7b8a4d2672
saehyen <rlatpgus4564@google.com>
2021-06-10 00:08:59 +0900

    이진탐색 정답(오래걸림)

---
+++ Numbercard2.py
@@ -0,0 +1,23 @@
+n = int(input())
+a = list(map(int, input().split()))
+a.sort()
+
+# 이진탐색 함수 정의
+def binary_search(num):
+    l = 0
+    r = n-1
+    while l <= r :
+        mid = (l+r)//2
+        if a[mid] == num :
+            return 1
+        elif a[mid] > num :
+            r = mid - 1
+            # 반 줄여주기 1
+        else:
+            l = mid + 1
+            # 반 줄여주기 2
+    return 0
+
+input()
+for num in list(map(int, input().split())):
+    print(binary_search(num), end = ' ')
