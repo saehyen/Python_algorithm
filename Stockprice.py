6280afc601e97dd20560995a663d3828c21966bc
saehyen <rlatpgus4564@google.com>
2021-06-17 16:10:51 +0900

    주식가격_2중for문사용

---
+++ Stockprice.py
@@ -0,0 +1,10 @@
+def solution(prices):
+    answer = [0] * len(prices)
+
+    for i in range(len(prices)-1):
+        for j in range(i, len(prices)-1):
+            if prices[i] >prices[j]:
+                break
+            else:
+                answer[i] +=1
+    return answer
