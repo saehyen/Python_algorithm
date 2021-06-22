0ace57347ebbaf6b0d87a9c6129c339d140f24d8
saehyen <rlatpgus4564@google.com>
2021-06-22 20:29:42 +0900

    가장 마음에 드는 식

---
+++ Keypad3.py
@@ -0,0 +1,29 @@
+
+def solution(numbers, hand):
+    answer = ''
+    location = [[3, 1], [0, 0], [0, 1], [0, 2], [1, 0], [1, 1], [1, 2], [2, 0], [2, 1], [2, 2]]
+    left, right = [3, 0], [3, 2]
+    for i in numbers:
+        if i % 3 == 1:
+            answer += 'L'
+            left = location[i]
+        elif i % 3 == 0 and i != 0:
+            answer += 'R'
+            right = location[i]
+        else:
+            l = abs(location[i][0] - left[0]) + abs(location[i][1] - left[1])
+            r = abs(location[i][0] - right[0]) + abs(location[i][1] - right[1])
+            if l < r:
+                answer += 'L'
+                left = location[i]
+            elif l > r:
+                answer += 'R'
+                right = location[i]
+            else:
+                answer += hand[0].upper()
+                if hand == 'right':
+                    right = location[i]
+                else:
+                    left = location[i]
+
+    return answer
