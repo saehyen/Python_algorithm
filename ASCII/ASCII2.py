f08801f88cc6daf8c60e59e1475ff4f4df563f00
saehyen <rlatpgus4564@naver.com>
2021-07-01 14:01:02 +0900

    대소문자 리스트로 구현한 답

---
+++ ASCII/ASCII2.py
@@ -0,0 +1,16 @@
+def caesar(s, n):
+    lower_list = "abcdefghijklmnopqrstuvwxyz"
+    upper_list = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+    result = []
+
+    for i in s:
+        if i is " ":
+            result.append(" ")
+        elif i.islower() is True:
+            new_ = lower_list.find(i) + n
+            result.append(lower_list[new_ % 26])
+        else:
+            new_ = upper_list.find(i) + n
+            result.append(upper_list[new_ % 26])
+    return "".join(result)
