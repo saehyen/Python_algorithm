a4131c6778af19418520dee8bef1e7bc47a42231
saehyen <rlatpgus4564@naver.com>
2021-07-02 00:32:16 +0900

    간단하게 구현한 답

---
+++ LCM GCF/LCM GCF2.py
@@ -0,0 +1,10 @@
+# 간단하게 구현한 답
+def gcdlcm(a, b):
+    c, d = max(a, b), min(a, b)
+    t = 1
+    while t > 0:
+        t = c % d
+        c, d = d, t
+    answer = [c, int(a*b/c)]
+
+    return answer
